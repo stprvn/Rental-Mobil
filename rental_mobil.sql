c27626d2ecfc97e55cd32a817aabe030a9add5d0
Stephanus Parveen <stparveen.14116128@student.itera.ac.id>
2018-04-22 17:04:46 +0700

    update sql

--- rental_mobil.sql
+++ rental_mobil.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 21 Apr 2018 pada 13.16
+-- Generation Time: 22 Apr 2018 pada 12.01
 -- Versi Server: 10.1.30-MariaDB
 -- PHP Version: 5.6.33
 
@@ -38,6 +38,21 @@ CREATE TABLE `driver` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `history`
+--
+
+CREATE TABLE `history` (
+  `id_penyewaan` int(5) NOT NULL,
+  `tanggal_sewa` date NOT NULL,
+  `tanggal_balik` date DEFAULT NULL,
+  `lama_sewa` int(3) NOT NULL,
+  `harga_sewa` int(10) NOT NULL,
+  `biaya_denda` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `member`
 --
 
@@ -58,6 +73,34 @@ CREATE TABLE `mobil` (
   `no_polisi` char(8) NOT NULL,
   `nama_mobil` varchar(20) NOT NULL,
   `transmisi` char(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `kode_bayar` int(7) NOT NULL,
+  `id_penyewaan` int(5) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `status` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penyewaan`
+--
+
+CREATE TABLE `penyewaan` (
+  `id_penyewaan` int(5) NOT NULL,
+  `tanggal_sewa` date NOT NULL,
+  `username_penyewa` char(10) NOT NULL,
+  `id_driver` char(4) DEFAULT NULL,
+  `harga` int(10) NOT NULL,
+  `lama_sewa` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -83,6 +126,18 @@ ALTER TABLE `mobil`
   ADD PRIMARY KEY (`no_polisi`);
 
 --
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`kode_bayar`);
+
+--
+-- Indexes for table `penyewaan`
+--
+ALTER TABLE `penyewaan`
+  ADD PRIMARY KEY (`id_penyewaan`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -91,6 +146,18 @@ ALTER TABLE `mobil`
 --
 ALTER TABLE `driver`
   MODIFY `id_driver` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `kode_bayar` int(7) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `penyewaan`
+--
+ALTER TABLE `penyewaan`
+  MODIFY `id_penyewaan` int(5) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
